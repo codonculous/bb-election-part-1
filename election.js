ca4f34566adaaa7327d20e66c7d9860a46caa3eb
codonculous <feidameng01@gmail.com>
2017-05-02 17:17:07 -0400

    add ajax to request data in json from https://bb-election-api.herokuapp.com/

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
-  
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      // data:
+      dataType: 'json'
+      
+    }).done(function(responseData){
+      console.log('data: '+ responseData);
+
+      for (var i=0;i<responseData.candidates.length;i++) {
+        $('#election-results').append('<li>'+'name: '+ responseData.candidates[i].name + ', votes: '+ responseData.candidates[i].votes + '</li>');
+      }
+
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      console.log('error: '+errorThrown);
+    });
 
 });
