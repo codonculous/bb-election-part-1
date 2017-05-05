49c2428e3c1c4b773d1022ea6e5932a7b864fb9b
codonculous <feidameng01@gmail.com>
2017-05-05 07:08:00 -0400

    create form in js allowing users to vote

--- election.js
+++ election.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
 
       for (var i=0;i<responseData.candidates.length;i++) {
         $('#election-results').append('<li>'+'name: '+ responseData.candidates[i].name + ', votes: '+ responseData.candidates[i].votes + '</li>');
+        var voting_form = $("<form class='voting-form' action='https://bb-election-api.herokuapp.com/vote' method='POST'></form>");
+        $(voting_form).append("<input type='hidden' name='name' value="+responseData.candidates[i].name+">");
+        $(voting_form).append("<input type='submit' value='Submit'>");
+        $('#election-results').append(voting_form);
       }
 
     }).fail(function(jqXHR, textStatus, errorThrown){
